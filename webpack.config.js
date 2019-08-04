1c1a6f4cb9676c3e251817f60d9fc2116e954fe8
miudit <dai0531m@gmail.com>
2019-08-04 14:17:45 +0900

    add models

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,17 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, '/dist'),
+    path: path.join(__dirname, './dist'),
+    publicPath: "/dist/",
     filename: 'bundle.js'
   },
   resolve: {
     modules: [
-      path.join(__dirname, 'assets'),
       "node_modules"
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, './src/index.html'),
-    })
-  ],
   module: {
     rules: [
       {
@@ -26,12 +20,24 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
-      }, 
-      { 
-        test: /\.(png|woff|woff2|eot|ttf|svg|...)$/, 
-        use: { 
-          loader: 'url-loader?limit=100000', 
-        }, 
+      },
+      {
+        test: /\.scss/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [path.join(__dirname, 'src/styles')],
+            }
+          }
+        ]
       },
     ]
   }
